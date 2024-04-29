506b57dce3b734d31afcaeff3d284ca79e7bd998
Urs Kober <urs.kober@mailbox.tu-dresden.de>
2024-04-29 17:07:56 +0200

    move visual info to separate file

---
+++ src/visual_info.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <cstdint>
+#include <cstdio>
+#include <map>
+#include <vector>
+
+struct ec_info_rw {
+    uint64_t time2;
+    uint64_t instr2;
+    uint64_t instr2_abs;
+    bool write_no_read;
+
+    static bool ec_info_rw_cmp(const ec_info_rw& eci, uint64_t time)
+    {
+        return eci.time2 < time;
+    }
+};
+
+struct visual_sample {
+    uint64_t addr;
+    uint64_t idx;
+    uint64_t hitc;
+    uint64_t combination;
+};
+
+class visual_info {
+
+  public:
+
+    uint64_t start_time;
+    uint64_t end_time;
+
+    std::map<uint64_t, std::vector<ec_info_rw>> ecs;
+
+    std::vector<visual_sample> samples;
+
+  private:
+
+    void serialize_u64(uint8_t* buf, uint64_t v);
+    uint64_t deserialize_u64(uint8_t* buf);
+    void write_out_u64(FILE* fstream, uint64_t v);
+    uint64_t read_in_u64(FILE* fstream);
+
+  public:
+
+    bool write_to_file(const char* filepath);
+    bool read_from_file(const char* filepath);
+};
